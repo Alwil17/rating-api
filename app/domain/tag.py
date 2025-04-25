6157fda859136a261addfe1811867705be972e8c
alwil17 <willialfred24@gmail.com>
2025-04-25 16:56:49 +0000

    Added tag model

---
+++ app/domain/tag.py
@@ -0,0 +1,17 @@
+# app/domain/models/tag.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.infrastructure.database import Base
+from .item_tag import item_tag
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+
+    items = relationship(
+        "Item",
+        secondary=item_tag,
+        back_populates="tags"
+    )
